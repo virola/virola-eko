f2ff88a2a85ecc7570139bd704aa26e9c8cf5ab8
virola <wetsky90@126.com>
2023-04-01 10:16:00 +0800

    adjust shell

--- virola.me/live.sh
+++ virola.me/live.sh
@@ -52,20 +52,20 @@ folder=$defFolder
 # read -p "是否需要为视频添加水印?水印位置默认在右上方,需要较好CPU支持(yes/no):" watermark
 
 watermark="no"
-if [ $watermark = "yes" ];then
-    read -p "输入你的水印图片存放绝对路径,例如/opt/image/watermark.jpg (格式支持jpg/png/bmp):" image
-    echo -e "${yellow} 添加水印完成,程序将开始推流. ${font}"
-    # 循环
-    while true
-    do
-        cd $folder
-        for video in $(ls *.mp4)
-        do
-        # ffmpeg -re -i "$video" -i "$image" -filter_complex overlay=W-w-5:5 -c:v libx264 -c:a aac -b:a 192k -strict -2 -f flv ${rtmp}
-        ffmpeg -re -i "$video" -c:v copy -c:a aac -ar 44100 -ac 1 -f flv ${rtmp}
-        done
-    done
-fi
+# if [ $watermark = "yes" ];then
+#     read -p "输入你的水印图片存放绝对路径,例如/opt/image/watermark.jpg (格式支持jpg/png/bmp):" image
+#     echo -e "${yellow} 添加水印完成,程序将开始推流. ${font}"
+#     # 循环
+#     while true
+#     do
+#         cd $folder
+#         for video in $(ls *.mp4)
+#         do
+#         # ffmpeg -re -i "$video" -i "$image" -filter_complex overlay=W-w-5:5 -c:v libx264 -c:a aac -b:a 192k -strict -2 -f flv ${rtmp}
+#         ffmpeg -re -i "$video" -c:v copy -c:a aac -ar 44100 -ac 1 -f flv ${rtmp}
+#         done
+#     done
+# fi
 if [ $watermark = "no" ]
 then
     echo -e "${yellow} 你选择不添加水印,程序将开始推流. ${font}"
@@ -74,9 +74,11 @@ then
     do
         cd $folder
         video=$(find ./ -type f | shuf -n 1)
-        # 上面这个命令直播流太卡了，不行用
+        # 主机带宽不够，推流有点卡顿
+        # -b:v 1000k -bufsize 1000k -maxrate 1500k
+        # 改为限制码率最大 1500k 之后，直播就不卡顿了
         # ffmpeg -re -i "$video" -preset ultrafast -vcodec libx264 -g 60 -b:v 2000k -c:a aac -b:a 128k -strict -2 -f flv ${rtmp}
-        ffmpeg -re -i "$video" -c:v copy -c:a aac -ar 44100 -ac 1 -f flv ${rtmp}
+        ffmpeg -re -i "$video" -preset ultrafast -vcodec libx264 -g 60 -b:v 1000k -bufsize 1000k -maxrate 1500k -c:a aac -b:a 128k -strict -2 -f flv ${rtmp}
     done
 fi
  }
